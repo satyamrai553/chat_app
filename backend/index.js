147868e5b97d49e9687982abe59a379ed2c2489d
Satyam Rai <satyamrai553@gmail.com>
2025-02-26 14:20:31 +0530

    basic socket io setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,25 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
 
-const io = new Server();
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
-io.on("connection", (socket) => {
-    console.log(socket)
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
-  
-  io.listen(3000,()=>{
-    console.log("server is listening on port 3000");
-    
-  });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
